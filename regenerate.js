d34ae9be511306a872b742d8421ae18c4ffdc0bf
Mathias Bynens <mathias@qiwi.be>
2017-09-18 16:17:44 +0200

    Release v1.3.3

--- regenerate.js
+++ regenerate.js
@@ -1,4 +1,4 @@
-/*! https://mths.be/regenerate v1.3.2 by @mathias | MIT license */
+/*! https://mths.be/regenerate v1.3.3 by @mathias | MIT license */
 ;(function(root) {
 
 	// Detect free variables `exports`.
@@ -1053,7 +1053,7 @@
 		return (new regenerate).add(value);
 	};
 
-	regenerate.version = '1.3.2';
+	regenerate.version = '1.3.3';
 
 	var proto = regenerate.prototype;
 	extend(proto, {
